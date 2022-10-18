a32db9c2eac501afd2cad8b8435504ca943ed8fc
Heo Jun <hjh1004@gmail.com>
2022-10-18 23:04:58 +0900

    Init: - Set up turborepo and yarn berry

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 200,
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+  };
+  
